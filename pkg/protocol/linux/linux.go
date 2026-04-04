2862d1647b0fc84f215145ab5add0e4c0e4b1d5f
Eric Hattemer <hattenator@gmail.com>
2026-04-04 16:35:29 -0700

    Fixed Empty Read Buffer
    Read does not allocate a buffer, but it does know how big the buffer is
    so I set an 8k buffer and it will throw errors if it exceeds that.

--- pkg/protocol/linux/linux.go
+++ pkg/protocol/linux/linux.go
@@ -15,17 +15,29 @@ type SocketListener struct {
 
 func (socketHolder SocketListener) ReceiveCommands() {
 	for true {
-		// TODO for security, I should limit the size of messages received.
-		var clientMessage []byte
+		const MAX_MESSAGE_SIZE = 8192
+		var clientMessage = make([]byte, MAX_MESSAGE_SIZE)
 		readSize, err := socketHolder.GuestSocket.Read(clientMessage)
-		if err == nil && readSize > 0 {
-			decodedMessage := protocol.CmdMessage{}
-			jsonErr := json.Unmarshal(clientMessage, &decodedMessage)
-			if jsonErr == nil {
-				log.Printf("Received Command: %v", decodedMessage)
-			}
-		}
+		if err == nil {
+			if readSize > 0 {
+				for readSize == MAX_MESSAGE_SIZE && err == nil {
+					log.Printf("Message Exceeded Maximum Size, Draining")
+					// If you see this, we could increase the MAX_MESSAGE_SIZE
+					readSize, err = socketHolder.GuestSocket.Read(clientMessage)
+				}
+				decodedMessage := protocol.CmdMessage{}
+				jsonErr := json.Unmarshal(clientMessage[:readSize], &decodedMessage)
+				if jsonErr == nil {
+					log.Printf("Received Command: %v", decodedMessage)
+				} else {
+					log.Printf("Json error when receiving from socket: %v", err)
+					log.Printf("Buffer was:\n%s", clientMessage[:readSize])
+				}
 
+			}
+		} else {
+			log.Printf("Error receiving commands from socket: %v", err)
+		}
 		time.Sleep(1000 * time.Millisecond)
 	}
 
